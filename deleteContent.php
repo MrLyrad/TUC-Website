2a435b671432d9f247a2cdee876a4da4ad45963f
SecreShall <164751004+SecreShall@users.noreply.github.com>
2024-04-27 18:03:51 +0800

    CMS admin side functional

---
+++ deleteContent.php
@@ -0,0 +1,38 @@
+<?php
+// Include database connection file
+include 'db-connector.php';
+
+    if (isset($_GET['id']))
+    {
+        $event_id = $_GET['id'];
+
+        //Prepare DELETE statement
+        $deleteRecord = "DELETE FROM events WHERE event_id = :event_id";
+
+        //Execute statement
+        $stmt = $pdo_obj->prepare($deleteRecord);
+        $stmt->execute(['event_id' => $event_id]);
+
+    // Check if deletion was successful
+    if($stmt->rowCount() > 0) {
+        // Redirect back to the page displaying all events
+        echo 
+        "
+        <script>
+        alert('Event Deleted');
+        </script>
+        ";
+        header("Location: contentDashboard.php");
+        exit();
+    } 
+    else 
+    {
+        echo "Failed to delete the event.";
+    }
+    } 
+    else 
+    {
+        echo "Invalid event ID.";
+    }
+
+?>
